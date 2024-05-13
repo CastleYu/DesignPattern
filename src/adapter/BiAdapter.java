45f16bd34cd0b25fa1bd7f5af04ffd1c389287cf
CastleYu <muziyuc@gmail.com>
2024-05-14 00:05:57 +0800

    双向适配器

--- src/adapter/BiAdapter.java
+++ src/adapter/BiAdapter.java
@@ -1,19 +1,34 @@
 package adapter;
 
-
+// 定义接口TargetA
 interface TargetA {
-
+    void performActionA();
 }
 
+// 定义接口TargetB
 interface TargetB {
-
+    void performActionB();
 }
 
+// Adapter类实现两个接口
 class Adapter implements TargetA, TargetB {
+    @Override
+    public void performActionA() {
+        System.out.println("Performing Action A");
+    }
 
+    @Override
+    public void performActionB() {
+        System.out.println("Performing Action B");
+    }
 }
 
-
-public class BiAdapter {
+// BiAdapter类继承自Adapter
+public class BiAdapter extends Adapter {
+    // 可以添加特定于BiAdapter的方法或属性
+    public void performBothActions() {
+        performActionA();
+        performActionB();
+    }
 }
 
